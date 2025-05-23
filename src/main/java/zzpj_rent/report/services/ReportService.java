5a9fca7b4a33d8dc5cd0ccc1791de9630576a6df
247659 <247659@edu.p.lodz.pl>
2025-05-23 15:23:33 +0200

    Update ReportService.java

--- src/main/java/zzpj_rent/report/services/ReportService.java
+++ src/main/java/zzpj_rent/report/services/ReportService.java
@@ -9,6 +9,7 @@ import java.util.*;
 import net.sf.jasperreports.engine.*;
 import zzpj_rent.report.dtos.request.ContractRequest;
 import zzpj_rent.report.exceptions.BadRequestException;
+import zzpj_rent.report.exceptions.ForbiddenException;
 import zzpj_rent.report.exceptions.NotFoundException;
 
 @Service
@@ -60,80 +61,91 @@ public class ReportService {
     }
 
     public JasperPrint createContract(ContractRequest request) throws JRException {
-        System.out.println("Contract request: " + request);
+        try {
+            System.out.println("Contract request: " + request);
 
-        if (request.getCity() == null || request.getCity().isEmpty()) {
-            throw new BadRequestException("City is required");
-        } else if (request.getLandlordIdNumber() == null || request.getLandlordIdNumber().isEmpty()) {
-            throw new BadRequestException("Landlord Id Number is required");
-        } else if (request.getLandlordIdNumber().length() != 9) {
-            throw new BadRequestException("Landlord Id Number must be 9 characters");
-        } else if (request.getTenantIdNumber() == null || request.getTenantIdNumber().isEmpty()) {
-            throw new BadRequestException("Tenant Id Number is required");
-        } else if (request.getTenantIdNumber().length() != 9) {
-            throw new BadRequestException("Tenant Id Number must be 9 characters");
-        } else if (request.getArea() == null) {
-            throw new BadRequestException("Area is required");
-        } else if (request.getArea() <= 0) {
-            throw new BadRequestException("Area is lower or equal to 0");
-        } else if (request.getFee() == null) {
-            throw new BadRequestException("Fee is required");
-        } else if (request.getFee() <= 0) {
-            throw new BadRequestException("Fee is lower or equal to 0");
-        } else if (request.getPayDay() > 31 || request.getPayDay() < 1) {
-            throw new BadRequestException("Pay day must be between 1 and 31");
-        } else if (request.getDeposit() == null) {
-            throw new BadRequestException("Deposit is required");
-        } else if (request.getDeposit() < 0) {
-            throw new BadRequestException("Deposit is lower than 0");
+            if (request.getCity() == null || request.getCity().isEmpty()) {
+                throw new BadRequestException("City is required");
+            } else if (request.getLandlordIdNumber() == null || request.getLandlordIdNumber().isEmpty()) {
+                throw new BadRequestException("Landlord Id Number is required");
+            } else if (request.getLandlordIdNumber().length() != 9) {
+                throw new BadRequestException("Landlord Id Number must be 9 characters");
+            } else if (request.getTenantIdNumber() == null || request.getTenantIdNumber().isEmpty()) {
+                throw new BadRequestException("Tenant Id Number is required");
+            } else if (request.getTenantIdNumber().length() != 9) {
+                throw new BadRequestException("Tenant Id Number must be 9 characters");
+            } else if (request.getArea() == null) {
+                throw new BadRequestException("Area is required");
+            } else if (request.getArea() <= 0) {
+                throw new BadRequestException("Area is lower or equal to 0");
+            } else if (request.getFee() == null) {
+                throw new BadRequestException("Fee is required");
+            } else if (request.getFee() <= 0) {
+                throw new BadRequestException("Fee is lower or equal to 0");
+            } else if (request.getPayDay() > 31 || request.getPayDay() < 1) {
+                throw new BadRequestException("Pay day must be between 1 and 31");
+            } else if (request.getDeposit() == null) {
+                throw new BadRequestException("Deposit is required");
+            } else if (request.getDeposit() < 0) {
+                throw new BadRequestException("Deposit is lower than 0");
+            }
+
+
+            String location = "Warszawa ul. Zamenhofa 31/15";
+            int rooms = 4;
+            Date nowDate = new Date();
+            LocalDate sinceLocalDate = LocalDate.now().plusDays(1);
+            LocalDate untilLocalDate = LocalDate.now().plusDays(5);
+
+            Date sinceDate = java.util.Date.from(sinceLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            Date untilDate = java.util.Date.from(untilLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+            if (location == null || location.isEmpty()) {
+                throw new NotFoundException("Location not found");
+            } else if (rooms == 0) {
+                throw new NotFoundException("Rooms not found");
+            } else if (nowDate == null) {
+                throw new ForbiddenException("NowDate not found");
+            } else if (sinceDate == null) {
+                throw new NotFoundException("SinceDate not found");
+            } else if (untilDate == null) {
+                throw new NotFoundException("UntilDate not found");
+            }
+
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("Data", nowDate);
+            parameters.put("location", location);
+            parameters.put("rooms", rooms);
+            parameters.put("city", request.getCity());
+            parameters.put("landlordName", "Dominik Gałkowski");
+            parameters.put("landlordCity", "Warszawa");
+            parameters.put("landlordAddress", "Sasanek 35");
+            parameters.put("landlordIdNumber", request.getLandlordIdNumber());
+            parameters.put("tenantName", "Marek Zawadzki");
+            parameters.put("tenantCity", "Warszawa");
+            parameters.put("tenantAddress", "Ciepła 35");
+            parameters.put("tenantIdNumber", request.getTenantIdNumber());
+            parameters.put("area", request.getArea());
+            parameters.put("sinceDate", sinceDate);
+            parameters.put("untilDate", untilDate);
+            parameters.put("fee", request.getFee());
+            parameters.put("payDay", request.getPayDay());
+            parameters.put("transmissionDate", sinceDate);
+            parameters.put("deposit", request.getDeposit());
+
+
+            String filePath = "reportsModels/Contract.jrxml";
+            InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+
+            if (reportStream == null) {
+                throw new NotFoundException("Plik raportu nie został znaleziony: " + filePath);
+            }
+
+            JasperReport report = JasperCompileManager.compileReport(reportStream);
+            return JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
+        } catch (JRException e) {
+            throw new BadRequestException("An error occurred while creating the contract: " + e.getMessage());
         }
-
-
-        String location = "Warszawa ul. Zamenhofa 31/15";
-        int rooms = 4;
-        Date nowDate = new Date();
-        LocalDate sinceLocalDate = LocalDate.now().plusDays(1);
-        LocalDate untilLocalDate = LocalDate.now().plusDays(5);
-
-        Date sinceDate = java.util.Date.from(sinceLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date untilDate = java.util.Date.from(untilLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-
-
-        //JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(user);
-
-        Map<String, Object> parameters = new HashMap<>();
-        //parameters.put("resourcesDataSet3", taskDataSource);
-        parameters.put("Data", nowDate);
-        parameters.put("location", location);
-        parameters.put("rooms", rooms);
-        parameters.put("city", request.getCity());
-        parameters.put("landlordName", "Dominik Gałkowski");
-        parameters.put("landlordCity", "Warszawa");
-        parameters.put("landlordAddress", "Sasanek 35");
-        parameters.put("landlordIdNumber", request.getLandlordIdNumber());
-        parameters.put("tenantName", "Marek Zawadzki");
-        parameters.put("tenantCity", "Warszawa");
-        parameters.put("tenantAddress", "Ciepła 35");
-        parameters.put("tenantIdNumber", request.getTenantIdNumber());
-        parameters.put("area", request.getArea());
-        parameters.put("sinceDate", sinceDate);
-        parameters.put("untilDate", untilDate);
-        parameters.put("fee", request.getFee());
-        parameters.put("payDay", request.getPayDay());
-        parameters.put("transmissionDate", sinceDate);
-        parameters.put("deposit", request.getDeposit());
-
-
-
-        String filePath = "reportsModels/Contract.jrxml";
-        InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
-
-        if (reportStream == null) {
-            throw new NotFoundException("Plik raportu nie został znaleziony: " + filePath);
-        }
-
-        JasperReport report = JasperCompileManager.compileReport(reportStream);
-        return JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
     }
 }
